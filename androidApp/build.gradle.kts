6942f2b1b89f6334b5a5c9ee5a7da7dec1237d01
chinarbhandari <chinarbhandari@gmail.com>
2022-02-21 23:47:07 +0530

    Basic dependencies and project structure defined

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -21,7 +23,29 @@ android {
 
 dependencies {
     implementation(project(":shared"))
+
+    // Android Support Dependencies
     implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+
+    //NAVIGATION
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.4.1")
+
+    //COMPOSE
+    implementation("androidx.compose.ui:ui:1.1.0")
+    implementation("androidx.compose.material:material:1.1.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.1.0")
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    //DI
+    implementation("io.insert-koin:koin-core:3.1.2")
+    implementation("io.insert-koin:koin-test:3.1.2")
+    implementation("io.insert-koin:koin-test-junit4:3.1.2")
+    implementation("io.insert-koin:koin-android:3.1.2")
+    implementation("io.insert-koin:koin-androidx-compose:3.1.2")
 }
---
+++ androidApp/src/main/java/com/chinar/electroluxapp/android/ui/activities/imagegallery/ImageGalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.chinar.electroluxapp.android.ui.activities.imagegallery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chinar.electroluxapp.android.R
+
+class ImageGalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_gallery)
+    }
+}
